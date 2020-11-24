68809210bcb0ddd6ba5ff79369f5e04bd4e6a744
chyatt <craig@chyatt.ca>
2020-11-24 16:18:20 +0000

    Now with script to create scrabble tiles, shuffle them, and store them in bag.txt for later use

--- fill-bag.py
+++ fill-bag.py
@@ -1,5 +1,18 @@
-bag = ['A1','A1','A1','A1','A1','A1','A1','A1','A1',
-       'B3','B3',
-       'C3','C3']
+import random
 
-print(bag)
+# Default list of letter tiles in Scrabble
+tiles = ['A1','A1','A1','A1','A1','A1','A1','A1','A1',
+       'B3','B3',
+       'C3','C3',
+       'D2','D2','D2','D2',
+       'E1','E1','E1','E1','E1','E1','E1','E1','E1','E1','E1', 'E1',
+       'F4','F4',
+       'G2','G2','G2',
+       'H4','H4']
+# TODO - Add the rest of the tiles!
+
+# Shuffle tiles and add them to bag.txt for later use
+random.shuffle(tiles)
+with open('bag.txt', 'w') as bag:
+    for tile in tiles:
+        bag.write("{}\n".format(tile))
