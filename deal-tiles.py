f6721b597cc17288b3039ee45cf79b45bf95a061
chyatt <craig@chyatt.ca>
2020-12-12 18:43:45 +0000

    Now with letter tiles printed to stdout for use with github actions

--- deal-tiles.py
+++ deal-tiles.py
@@ -11,3 +11,4 @@ dealtTiles, tiles = tiles[:int(sys.argv[1])], tiles[int(sys.argv[1]):]
 # TODO - Fix hard coded filenames
 utils.writeBag(tiles,'bag.txt')
 utils.logTurn(','.join(dealtTiles),'log.txt')
+print(','.join(dealtTiles))
